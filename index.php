950208e5f930fd7eab5fa56c0aac9c615f2ece8e
alanacpereira <alana.c.pereira@aluno.senai.br>
2025-11-27 10:57:44 -0300

    CSS PHP
    Aplicando meu estilo visual personalizado

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
         "Meu Jogo Favorito" => "https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR",
         "Música do Momento" => "https://www.youtube.com/watch?v=BW73u2yGGa8&list=RDBW73u2yGGa8&start_radio=1"
     ];
+
+    $link_vip = "WhatsApp";
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,23 +17,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BioLink de <?php echo $nome; ?></title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div class="container">
-   <img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="foto">
-   <h1> <?php echo $nome; ?> </h1>
-   <p> <?php echo $bio; ?> </p>
-   <div class="lista-links">
-       <?php
-            foreach ($links as $texto => $url) {
-                $classe_extra = "";
-                echo "<a href='$url' class='btn $classe_extra' target='_blank'><br>
+    
+    <div class="container">
+        
+         <img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="foto">
+
+         <h1> <?php echo $nome; ?> </h1>
+         <p> <?php echo $bio; ?> </p>
+
+        <div class="lista-links">
+            <?php
+                foreach ($links as $texto => $url) {
+                     $classe_extra = ($texto == $link_vip) ? "destaque" : "";
+                     echo "<a href='$url' class='btn $classe_extra' target='_blank'><br>
                         $texto
                       </a>";
-            }
+                 }
             ?>
-   </div>
-</div>
+        </div>
 
+        <div class="area-contato">
+            <a href="contato.php" class="btn-msg">Enviar Mensagem</a>
+        </div>    
+
+     </div>
 </body>
 </html>
