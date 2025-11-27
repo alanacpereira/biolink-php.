54fcf23af86d4efca15514d6a0040cda1194ab16
alanacpereira <alana.c.pereira@aluno.senai.br>
2025-11-27 10:27:26 -0300

    Biolink php
    Criando loop com meus links pessoais

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
     $nome = "Alana Camilli";
     $bio = " 18, summers<br>ce, Brasil<br>🐚🏐🍃🏝️🌟🌊<br> here we go again";
     $imagem = "https://i.pinimg.com/736x/28/cc/19/28cc19385dad8d5af02097eda648c524.jpg";
+    $links = [
+        "Instagram" => "https://instagram.com/@eu.alanacamilli",
+        "WhatsApp" => "https://wa.me/55+ 17 991012525",
+        "Meu Jogo Favorito" => "https://play.google.com/store/apps/details?id=com.dts.freefireth&hl=pt_BR",
+        "Música do Momento" => "https://www.youtube.com/watch?v=BW73u2yGGa8&list=RDBW73u2yGGa8&start_radio=1"
+    ];
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -15,7 +21,16 @@
    <img src="<?php echo $imagem; ?>" alt="Foto de Perfil" class="foto">
    <h1> <?php echo $nome; ?> </h1>
    <p> <?php echo $bio; ?> </p>
-   <div class="lista-links"></div>
+   <div class="lista-links">
+       <?php
+            foreach ($links as $texto => $url) {
+                $classe_extra = "";
+                echo "<a href='$url' class='btn $classe_extra' target='_blank'><br>
+                        $texto
+                      </a>";
+            }
+            ?>
+   </div>
 </div>
 
 </body>
